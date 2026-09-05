8954edbb8a293b5e00be31f57a3dee9b9df77603
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-05 22:07:38 +0800

    feat(ui): register dashboard editor locale

--- yak-ops-ui/src/locales/en-US.ts
+++ yak-ops-ui/src/locales/en-US.ts
@@ -1,6 +1,7 @@
 import batchLinkUp from './en-US/batch-link-up';
 import component from './en-US/component';
 import dashboard from './en-US/dashboard';
+import dashboardEditor from './en-US/dashboard-editor';
 import dataDevelopment from './en-US/data-development';
 import dataDevelopmentEditor from './en-US/data-development-editor';
 import dataQuality from './en-US/data-quality';
@@ -50,4 +51,5 @@ export default {
   ...workflowInstance,
   ...workflowScheduleHistory,
   ...dashboard,
+  ...dashboardEditor,
 };
