fb1d8bab890f1c2662e912e37c702c68ead41698
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-24 14:35:41 +0800

    i18n(ui): add datasource clear search label

--- yak-ops-ui/src/pages/data-source/i18n/messages.ts
+++ yak-ops-ui/src/pages/data-source/i18n/messages.ts
@@ -62,6 +62,7 @@ export default {
   'pages.datasource.group.search': '搜索引擎',
   'pages.datasource.typeSelector.title': '选择数据源',
   'pages.datasource.typeSelector.searchPlaceholder': '搜索数据源',
+  'pages.datasource.typeSelector.clearSearch': '清空搜索',
   'pages.datasource.typeSelector.allCategories': '全部分类',
   'pages.datasource.typeSelector.common': '常用',
   'pages.datasource.typeSelector.allDataSources': '全部数据源',
