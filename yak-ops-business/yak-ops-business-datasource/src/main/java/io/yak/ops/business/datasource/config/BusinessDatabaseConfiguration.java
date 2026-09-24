8559d09362d910d45119d18f5a5bc4f28cdfe8b6
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-24 15:19:35 +0800

    refactor(dao): centralize Flyway runtime

--- yak-ops-business/yak-ops-business-datasource/src/main/java/io/yak/ops/business/datasource/config/BusinessDatabaseConfiguration.java
+++ yak-ops-business/yak-ops-business-datasource/src/main/java/io/yak/ops/business/datasource/config/BusinessDatabaseConfiguration.java
@@ -17,6 +17,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -69,6 +70,7 @@ public class BusinessDatabaseConfiguration {
     }
 
     @Primary
+    @DependsOn("yakOpsFlyway")
     @Bean(
             name = {
                     "yakBusinessSqlSessionFactory",
