c02bca0ba58398352f862f419b89d8c2e58540c9
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-24 18:52:07 +0800

    refactor(java): migrate Spring injection to Resource

--- yak-ops-boot/src/main/java/io/yak/ops/boot/controller/datasource/v1/DataSourcePluginConfigController.java
+++ yak-ops-boot/src/main/java/io/yak/ops/boot/controller/datasource/v1/DataSourcePluginConfigController.java
@@ -1,5 +1,6 @@
 package io.yak.ops.boot.controller.datasource.v1;
 
+import jakarta.annotation.Resource;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.yak.ops.boot.controller.datasource.v1.converter.DataSourcePluginViewConverter;
@@ -8,7 +9,6 @@ import io.yak.ops.business.datasource.query.DataSourcePluginReader;
 import io.yak.ops.common.Result;
 import io.yak.ops.common.bean.vo.datasource.DataSourcePluginConfigVO;
 import io.yak.ops.common.constant.datasource.DataSourceConstants;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "数据源表单配置接口")
 @RestController
 @ConditionalOnDataSourceEnabled
-@RequiredArgsConstructor
 @RequestMapping(DataSourceConstants.API_PREFIX + "/plugin/config")
 public class DataSourcePluginConfigController {
-    private final DataSourcePluginReader pluginReader;
-    private final DataSourcePluginViewConverter viewConverter;
+    @Resource
+    private DataSourcePluginReader pluginReader;
+    @Resource
+    private DataSourcePluginViewConverter viewConverter;
 
     @Operation(summary = "查询数据源动态表单配置")
     @GetMapping
