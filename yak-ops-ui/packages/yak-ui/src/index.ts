424e2c8508f0f9d8f776ccc61e7abb397e1209ef
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-24 16:11:05 +0800

    feat(ui): expose Combobox primitive

--- yak-ops-ui/packages/yak-ui/src/index.ts
+++ yak-ops-ui/packages/yak-ui/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./badge";
 export * from "./button";
 export * from "./collapsible";
+export * from "./combobox";
 export * from "./dialog";
 export * from "./drawer";
 export * from "./dropdown-menu";
