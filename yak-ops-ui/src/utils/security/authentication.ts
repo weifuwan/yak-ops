5f7b5cdb5068d1b977991b44ea19ad7d3e44254c
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-24 13:36:54 +0800

    refactor: remove legacy login utilities

--- yak-ops-ui/src/utils/security/authentication.ts
+++
@@ -1,6 +0,0 @@
-export const AUTHENTICATION_INVALIDATED_EVENT =
-  'yak-security:authentication-invalidated';
-
-export const dispatchAuthenticationInvalidated = () => {
-  window.dispatchEvent(new Event(AUTHENTICATION_INVALIDATED_EVENT));
-};
