63125f357503b438323fd35be65448d39a5023d8
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-24 14:34:10 +0800

    refactor(ui): remove datasource YakButton export

--- yak-ops-ui/src/pages/data-source/components/ui/index.ts
+++ yak-ops-ui/src/pages/data-source/components/ui/index.ts
@@ -1,4 +1,2 @@
-export { default as YakButton } from "./YakButton";
-export type { YakButtonProps } from "./YakButton";
 export { default as YakTab } from "./YakTab";
 export type { YakTabProps } from "./YakTab";
