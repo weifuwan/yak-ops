f4bb104278bb3c32719a212855be2b44125dbdcb
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-25 17:43:39 +0800

    docs(plugin): standardize oracle package documentation

--- yak-ops-plugins/yak-ops-plugin-datasource/yak-ops-plugin-datasource-jdbc/src/main/java/io/yak/ops/plugin/database/jdbc/oracle/package-info.java
+++ yak-ops-plugins/yak-ops-plugin-datasource/yak-ops-plugin-datasource-jdbc/src/main/java/io/yak/ops/plugin/database/jdbc/oracle/package-info.java
@@ -1,2 +1,7 @@
-/** Oracle JDBC database plugin implementation boundary. */
+/**
+ * Oracle JDBC Provider 的数据库特有实现边界。
+ *
+ * @author weifuwan
+ * @since 2026-09-24
+ */
 package io.yak.ops.plugin.database.jdbc.oracle;
