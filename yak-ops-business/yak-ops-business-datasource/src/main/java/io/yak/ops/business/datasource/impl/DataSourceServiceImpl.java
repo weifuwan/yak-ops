4ee673cb9ab0145788e9299336e0806c695612e0
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-25 16:25:52 +0800

    refactor(datasource): remove summary service contract

--- yak-ops-business/yak-ops-business-datasource/src/main/java/io/yak/ops/business/datasource/impl/DataSourceServiceImpl.java
+++ yak-ops-business/yak-ops-business-datasource/src/main/java/io/yak/ops/business/datasource/impl/DataSourceServiceImpl.java
@@ -8,14 +8,12 @@ import io.yak.ops.business.datasource.plugin.DataSourcePluginRegistry;
 import io.yak.ops.common.bean.dto.datasource.DataSourceConnectTestDTO;
 import io.yak.ops.common.bean.dto.datasource.DataSourceDTO;
 import io.yak.ops.common.bean.dto.datasource.DataSourceQueryDTO;
-import io.yak.ops.common.bean.vo.datasource.DataSourceSummaryVO;
 import io.yak.ops.common.bean.vo.datasource.DataSourceVO;
 import io.yak.ops.common.enums.datasource.DataSourceConnStatus;
 import io.yak.ops.common.enums.datasource.DataSourceEnvironment;
 import io.yak.ops.common.enums.datasource.DataSourceErrorCode;
 import io.yak.ops.common.page.PagingData;
 import io.yak.ops.dao.entity.datasource.DataSourceEntity;
-import io.yak.ops.dao.model.datasource.DataSourceSummaryRow;
 import io.yak.ops.dao.repository.datasource.DataSourceEntityRepository;
 import io.yak.ops.dao.repository.datasource.DataSourcePageQuery;
 import jakarta.annotation.Resource;
@@ -130,10 +128,6 @@ public class DataSourceServiceImpl implements DataSourceService {
         return PagingData.from(repository.queryPage(query).map(value -> toDataSourceVO(value, false)));
     }
 
-    @Override
-    public DataSourceSummaryVO queryDataSourceSummary() {
-        return toSummaryVO(repository.querySummary());
-    }
 
     @Override
     public boolean testConnection(String id) {
@@ -267,15 +261,4 @@ public class DataSourceServiceImpl implements DataSourceService {
         return target;
     }
 
-    private DataSourceSummaryVO toSummaryVO(DataSourceSummaryRow source) {
-        if (source == null) {
-            return new DataSourceSummaryVO(0L, 0L, 0L, 0L, 0L);
-        }
-        return new DataSourceSummaryVO(
-                source.getTotal(),
-                source.getConnected(),
-                source.getDisconnected(),
-                source.getUnknown(),
-                source.getEnvironmentCount());
-    }
 }
