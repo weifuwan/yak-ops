f6debe0085b9cda1212fbe20486332d6cbd1658b
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-25 17:43:33 +0800

    docs(plugin): standardize jdbc package documentation

--- yak-ops-plugins/yak-ops-plugin-datasource/yak-ops-plugin-datasource-jdbc/src/main/java/io/yak/ops/plugin/database/jdbc/package-info.java
+++ yak-ops-plugins/yak-ops-plugin-datasource/yak-ops-plugin-datasource-jdbc/src/main/java/io/yak/ops/plugin/database/jdbc/package-info.java
@@ -1,2 +1,7 @@
-/** Shared JDBC support used by concrete relational database plugins. */
+/**
+ * JDBC Provider 共享实现，负责连接参数规范化、连接创建、SSH 隧道和 Catalog 元数据访问。
+ *
+ * @author weifuwan
+ * @since 2026-09-24
+ */
 package io.yak.ops.plugin.database.jdbc;
