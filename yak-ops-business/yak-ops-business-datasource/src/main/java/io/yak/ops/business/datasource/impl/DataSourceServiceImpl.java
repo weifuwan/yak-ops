b1f460f1c302d68706ee2e305994366577e677ce
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-25 16:48:26 +0800

    refactor(datasource): remove service capability conditional

--- yak-ops-business/yak-ops-business-datasource/src/main/java/io/yak/ops/business/datasource/impl/DataSourceServiceImpl.java
+++ yak-ops-business/yak-ops-business-datasource/src/main/java/io/yak/ops/business/datasource/impl/DataSourceServiceImpl.java
@@ -1,7 +1,6 @@
 package io.yak.ops.business.datasource.impl;
 
 import io.yak.ops.business.datasource.DataSourceService;
-import io.yak.ops.business.datasource.config.ConditionalOnDataSourceEnabled;
 import io.yak.ops.business.datasource.config.DataSourceProperties;
 import io.yak.ops.business.datasource.exception.DataSourceException;
 import io.yak.ops.business.datasource.plugin.DataSourcePluginRegistry;
@@ -28,7 +27,6 @@ import org.springframework.util.StringUtils;
  * @since 2026-09-25
  */
 @Service
-@ConditionalOnDataSourceEnabled
 public class DataSourceServiceImpl implements DataSourceService {
 
     @Resource
