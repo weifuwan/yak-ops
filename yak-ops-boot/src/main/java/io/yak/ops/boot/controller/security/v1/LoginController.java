30f7d8b47d6cc52e292c971e0cc32917b94ac78a
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-09-25 17:04:51 +0800

    refactor(security): unify controller null handling

--- yak-ops-boot/src/main/java/io/yak/ops/boot/controller/security/v1/LoginController.java
+++ yak-ops-boot/src/main/java/io/yak/ops/boot/controller/security/v1/LoginController.java
@@ -9,6 +9,7 @@ import io.yak.ops.common.enums.security.ResultCode;
 import io.yak.ops.common.exception.YakSecurityException;
 import io.yak.ops.common.result.Result;
 import io.yak.ops.common.util.ObjectUtils;
+import io.yak.ops.common.util.ObjectUtils;
 import io.yak.ops.security.authentication.AuthenticationManager;
 import io.yak.ops.security.constant.SecurityConstants;
 import io.yak.ops.security.service.LoginService;
